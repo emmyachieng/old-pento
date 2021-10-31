0561bd7dc449bbb866451e84057f9370e24b0602
emmyachieng <emmyachieng97@gmail.com>
2021-10-31 23:41:31 +0300

    generated a catalog context and product resource

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Pento.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:bcrypt_elixir, "~> 2.0"},
+      {:bcrypt_elixir, "~> 2.0.3"},
       {:phoenix, "~> 1.5.9"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
