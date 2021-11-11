74ad8e41576ec0e66e793b3f6366d2e225e79efd
emmyachieng <emmyachieng97@gmail.com>
2021-11-11 06:01:49 +0300

    stateless component for building survey tool for fetching demographic and rating information

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -23,6 +23,7 @@ defmodule PentoWeb.Router do
     live "/", PageLive, :index
     live "/guess", WrongLive
     live "/promo", PromoLive
+    live "/survey", SurveyLive, :index
     # Generated product routes start here:
     live "/products", ProductLive.Index, :index
     live "/products/new", ProductLive.Index, :new
