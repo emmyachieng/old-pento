9df1e2a267b07b458a6f3f432bb7d37d86e7e571
emmyachieng <emmyachieng97@gmail.com>
2021-11-12 04:36:43 +0300

    stateful component for the survey feature

---
+++ lib/pento_web/live/rating_live/show_component.ex
@@ -0,0 +1,18 @@
+defmodule PentoWeb.RatingLive.ShowComponent do
+    use PentoWeb, :live_component
+
+    def render_rating_stars(stars) do
+        filled_stars(stars)
+        |> Enum.concat(unfilled_stars(stars))
+        |> Enum.join(" ")
+    end
+
+    def filled_stars(stars) do
+        List.duplicate("<span class='fa fa-star checked'></span>", stars)
+    end
+
+    def unfilled_stars(stars) do
+        List.duplicate("<span class='fa fa-star'></span>", 5 - stars)
+    end
+
+end
